f259432fa7da54a8d6fe7bc0a4138584460018fc
Mojtaba <mojtabaparvizi19@gmail.com>
2024-07-13 17:11:16 -0400

    Add Apikey to env

--- public/services/apiClient.js
+++ public/services/apiClient.js
@@ -4,7 +4,6 @@ export default axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMWZkNGJjZDU1YTAzMGQwNWY1ZDI5MDQyZmEwNDc4ZiIsIm5iZiI6MTcyMDI4NDIzNS41OTMwODUsInN1YiI6IjY1ZGYzODc0N2YyZDRhMDE2MzY3N2JhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Cj6dUrtMqDkpbe62ILhwC7ckqEtSaYfW11BbAnnuiE0",
+    Authorization: process.env.APP_API,
   },
 });
