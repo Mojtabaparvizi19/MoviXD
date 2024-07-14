e80b90f3e37d81ba98c1a0d29b2b4fcc2d7d8562
Mojtaba <mojtabaparvizi19@gmail.com>
2024-07-14 08:42:02 -0400

    Fix Bugs

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const app = express();
 app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
