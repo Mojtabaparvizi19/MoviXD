41c3caeac230d75c7286f35c08be954915368e5d
Mojtaba <mojtabaparvizi19@gmail.com>
2024-07-16 13:57:53 -0400

    4th try

--- public/services/apiClient.js
+++ public/services/apiClient.js
@@ -5,6 +5,7 @@ export default axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
     accept: "application/json",
-    Authorization: process.env.APP_API,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMWZkNGJjZDU1YTAzMGQwNWY1ZDI5MDQyZmEwNDc4ZiIsInN1YiI6IjY1ZGYzODc0N2YyZDRhMDE2MzY3N2JhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pWxzEtvGhXFgYD8O_Ipr2hwqp3Fxw7PgHoNmFGk1qqI",
   },
 });
